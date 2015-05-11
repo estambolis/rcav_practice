e83b662815badfcc315671ffc05c0fb49ddd0c08
Elias Stambolis <estambol@chicagobooth.edu>
2015-05-10 23:13:42 -0500

    calcs working

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,30 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def square
+  number = params[:number].to_f
+  @squared = number**2
+  end
+
+
   def sqrt
+  number = params[:number].to_f
+  @sqrt = Math.sqrt(number).round(2)
+
+  render('square_root')
   end
 
   def pmt
+  int = params[:interest].to_f/10000
+  pmt = params[:payment_count].to_f
+  princ = params[:principal].to_f
+
+  numerator = int*princ
+  denominator = 1-(1+int)**(-pmt)
+  @pmt = "-$#{(numerator/denominator).round(2)}"
+
+  render('payment')
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:number", { :controller => "calculations", :action => "square" })
+  get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
+  get("/payment/:interest/:payment_count/:principal", {:controller => "calculations", :action => "pmt"})
 end
+
+
